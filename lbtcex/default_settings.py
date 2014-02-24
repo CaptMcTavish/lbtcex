4023633f0f8c11b7d5038d803fe4e06e7583c5de
Leo Honkanen <leo.o.honkanen@gmail.com>
2014-02-24 11:24:25 +0200

    add registration templates

--- lbtcex/default_settings.py
+++ lbtcex/default_settings.py
@@ -36,6 +36,11 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'registration',
+
+    'lbtcex.main',
+    'lbtcex.client',
 )
 
 MIDDLEWARE_CLASSES = (
