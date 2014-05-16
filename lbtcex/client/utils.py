92a8dfb744de3e53c0665d24e555d25d00f25cf2
Leo Honkanen <leo.o.honkanen@gmail.com>
2014-05-16 14:10:36 -0600

    Handle expired tokens using refresh tokens

--- lbtcex/client/utils.py
+++ lbtcex/client/utils.py
@@ -1,10 +1,13 @@
 import functools
+import logging
 
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
 from django.conf import settings
 
 import requests
+
+logger = logging.getLogger(__name__)
 
 
 def api_token_required(f):
@@ -16,16 +19,52 @@ def api_token_required(f):
         return f(request, *args, **kwargs)
     return requirer
 
+def catch_expired_token_and_retry(f):
+    """Tokens expire rather quickly, but we generally have a refresh token
+    to get new ones as necessary.
+
+    Try to get a new token and retry a invalid-token-failed call once.
+
+    """
+
+    @functools.wraps(f)
+    def wrapper(request, *args, **kwargs):
+        resp = f(request, *args, **kwargs)
+
+        resp_json = resp.json()
+
+        if "error" in resp_json and resp_json["error"]["error_code"] == 3:
+            # expired/invalid token, try to get a new one with our refresh token
+            logger.info("Invalid access token, trying to get a new one")
+
+            results = requests.post(
+                settings.LBTC_URL + "/oauth2/access_token/",
+                data={"grant_type": "refresh_token",
+                      "client_id": settings.LBTC_CLIENT_ID,
+                      "client_secret": settings.LBTC_CLIENT_SECRET,
+                      "refresh_token": request.user.profile.access_token_refresh_token,})
+
+            if results.status_code != 200:
+                # just return the original error
+                return resp
+
+            request.user.profile.set_access_token(**results.json())
+            request.user.profile.save()
+
+            return f(request, *args, **kwargs)
+
+        return resp
+
+    return wrapper
+
+@catch_expired_token_and_retry
 def api_get(request, path, params=None):
-    p = {"access_token": request.user.profile.access_token}
-    if params is not None:
-        p.update(params)
+    headers = {"Authorization": "Bearer " + request.user.profile.access_token}
 
-    return requests.get(settings.LBTC_URL + path, params=p)
+    return requests.get(settings.LBTC_URL + path, params=params, headers=headers)
 
+@catch_expired_token_and_retry
 def api_post(request, path, data=None):
-    d = {"access_token": request.user.profile.access_token}
-    if data is not None:
-        d.update(data)
+    headers = {"Authorization": "Bearer " + request.user.profile.access_token}
 
-    return requests.post(settings.LBTC_URL + path, data=d)
+    return requests.post(settings.LBTC_URL + path, data=data, headers=headers)
